bfaab2dcfd57e02c12e8ea00917dcc28c4f02661
Hayen Phan <hayenphan@outlook.com>
2020-03-14 19:08:02 +0100

    WIP styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import SvgFigure1 from './svg/Figure1';
 
 class Home extends React.Component {
   render() {
     return (
       <Container fluid>
-          <Row className="home">
-          <div className="home__slime"> </div>
-              <Col xs={12}>
-                  <Row className="home__menu">
-                      <Col xs={4}> intro </Col>
-                      <Col xs={4}> projects </Col>
-                      <Col xs={4}> contact </Col>
-                  </Row>
-              </Col>
-              <Col xs={12}>
-                  Hayen Phan
-              </Col>
-              <Col xs={12}>
-                  Ux/Ui
-              </Col>
-          </Row>
+          <section className="home">
+              <div className="home__slime"> </div>
+              <Row className="home__menu">
+                      <Col xs={4} md={1} className="home__menu--item"> contact </Col>
+                      <Col xs={4} md={1} className="home__menu--item"> projects </Col>
+                      <Col xs={4} md={1} className="home__menu--item"> intro </Col>
+              </Row>
+              <div className="home__info--wrapper">
+                      <Row className="home__info">
+                          <div className="home__info--name-wrapper">
+                              <Col className="home__info--name"> HAYEN </Col>
+                              <Col className="home__info--lastname"> PHAN </Col>
+                          </div>
+                          <Col className="home__info--spec">
+                              <div className="home__specialization">
+                                  <p className="home__specialization-title"> UI/UX </p>
+                              </div>
+                          </Col>
+                          <Col className="home__info--arrow">
+                          </Col>
+                      </Row>
+              </div>
+          </section>
       </Container>
     )
   }
