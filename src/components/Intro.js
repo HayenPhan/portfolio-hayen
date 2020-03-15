83c962b9426f9d1ed90bf6db69c5623381181eff
Hayen Phan <hayenphan@outlook.com>
2020-03-15 20:09:43 +0100

    WIP intro styling

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import PageNumber from './PageNumber';
 
 const Intro = () => {
     return(
         <section className="intro">
+            <PageNumber> </PageNumber>
+            <div className="intro__image-wrapper">
 
+            </div>
+            <div className="intro__text-wrapper">
+
+            </div>
         </section>
     )
 };
---
+++ src/components/PageNumber.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageNumber = () => {
+    return(
+        <div className="pagenumber">
+            <p> 01 - ditjsfos </p>
+        </div>
+    )
+};
+
+export default PageNumber;
