998c774f68b9a21cb03cce1660c2de0886f81cd0
Hayen Phan <hayenphan@outlook.com>
2020-05-11 09:32:41 +0200

    Updates

---
+++ precache-manifest.2a02818c871df40d31ee2ea809a02654.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5123e938951e08412e3c20d701db9d63",
+    "url": "/portfolio-hayen/index.html"
+  },
+  {
+    "revision": "d7ab3776584ff3098011",
+    "url": "/portfolio-hayen/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0602a4be81ab6b6d2be4",
+    "url": "/portfolio-hayen/static/css/main.3e500da5.chunk.css"
+  },
+  {
+    "revision": "d7ab3776584ff3098011",
+    "url": "/portfolio-hayen/static/js/2.ff78d5fb.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/portfolio-hayen/static/js/2.ff78d5fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0602a4be81ab6b6d2be4",
+    "url": "/portfolio-hayen/static/js/main.8900350c.chunk.js"
+  },
+  {
+    "revision": "5f042dd785d20d99101f",
+    "url": "/portfolio-hayen/static/js/runtime-main.3400913e.js"
+  },
+  {
+    "revision": "d819a366c5daf33db6cf6ce72cf939b3",
+    "url": "/portfolio-hayen/static/media/banzai.d819a366.jpg"
+  },
+  {
+    "revision": "f48083bee1678665f3ae8d12d97d7ada",
+    "url": "/portfolio-hayen/static/media/bottom_block--desktop.f48083be.png"
+  },
+  {
+    "revision": "7a1175561abb5a1a737260215aee5bac",
+    "url": "/portfolio-hayen/static/media/bottom_block--mobile.7a117556.png"
+  },
+  {
+    "revision": "14fcd21577fe663eca8d7a88050c98a1",
+    "url": "/portfolio-hayen/static/media/iphone_mockup.14fcd215.png"
+  },
+  {
+    "revision": "c453b16fe5b7dba3faf675f1d1a87d76",
+    "url": "/portfolio-hayen/static/media/macbook_mockup.c453b16f.png"
+  },
+  {
+    "revision": "50aec63687a31757b9345550990bc370",
+    "url": "/portfolio-hayen/static/media/phase--1.50aec636.png"
+  },
+  {
+    "revision": "a631b511a32a05d546c6a41eda944deb",
+    "url": "/portfolio-hayen/static/media/phase--2.a631b511.png"
+  },
+  {
+    "revision": "832787616f62eabe41fa6801e5288c2a",
+    "url": "/portfolio-hayen/static/media/phase--3.83278761.png"
+  },
+  {
+    "revision": "9193391fb89761d13c6f780c7cf93c3b",
+    "url": "/portfolio-hayen/static/media/phase--4.9193391f.png"
+  },
+  {
+    "revision": "8789175ee64c13d70483805a1a3e2a66",
+    "url": "/portfolio-hayen/static/media/picture--mobile.8789175e.png"
+  },
+  {
+    "revision": "68102816388b2ee4670235d0a25dd4f6",
+    "url": "/portfolio-hayen/static/media/right_block--desktop.68102816.png"
+  },
+  {
+    "revision": "efa4980bcf5d099676c8bc9905818669",
+    "url": "/portfolio-hayen/static/media/slime--mobile.efa4980b.png"
+  },
+  {
+    "revision": "ba1231c9f27183ca7624274d2c529835",
+    "url": "/portfolio-hayen/static/media/slime-specialization--mobile.ba1231c9.png"
+  },
+  {
+    "revision": "1a551b7e773908d78fa21255d8f4fd0f",
+    "url": "/portfolio-hayen/static/media/tropicana.1a551b7e.jpg"
+  },
+  {
+    "revision": "10e3878c7aa2ffc9cee9b2552069f12f",
+    "url": "/portfolio-hayen/static/media/wireframe--1.10e3878c.png"
+  },
+  {
+    "revision": "ff72fcc9ebdbb71e3107ce1ac2e9e46f",
+    "url": "/portfolio-hayen/static/media/wireframe--2.ff72fcc9.png"
+  },
+  {
+    "revision": "c9f735ce5e8c7f5df8893054604b7052",
+    "url": "/portfolio-hayen/static/media/wireframe--3.c9f735ce.png"
+  },
+  {
+    "revision": "5590c9026bf015b0e2927343dd5d631f",
+    "url": "/portfolio-hayen/static/media/wireframe--4.5590c902.png"
+  },
+  {
+    "revision": "62d4bf2c4413676ded16c16620dac126",
+    "url": "/portfolio-hayen/static/media/wireframebanzai--1.62d4bf2c.jpg"
+  },
+  {
+    "revision": "c08202f590537954bf7025475d7dc2e1",
+    "url": "/portfolio-hayen/static/media/wireframebanzai--2.c08202f5.jpg"
+  },
+  {
+    "revision": "10336f23c5d55ecbddafedc3973a0a18",
+    "url": "/portfolio-hayen/static/media/wireframebanzai--3.10336f23.jpg"
+  },
+  {
+    "revision": "582856f50eab3d9122fcea88edf4ffd0",
+    "url": "/portfolio-hayen/static/media/wireframebanzai--4.582856f5.jpg"
+  }
+]);
