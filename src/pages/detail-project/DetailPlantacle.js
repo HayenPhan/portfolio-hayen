d67945e826aa43d833a71cf413a7535f5a9f10a8
Hayen Phan <hayenphan@MacBook-Pro-van-Hayen.local>
2020-07-07 16:53:02 +0200

    Added new text block

--- src/pages/detail-project/DetailPlantacle.js
+++ src/pages/detail-project/DetailPlantacle.js
@@ -7,6 +7,7 @@ import { Container } from 'react-bootstrap';
 import Intro from './sections/Intro';
 import PhasesPlantacle from './sections/PhasesPlantacle';
 import Wireframes from './sections/Wireframes';
+import Role from './sections/Role';
 
 
 class DetailPlantacle extends React.Component {
@@ -22,6 +23,11 @@ class DetailPlantacle extends React.Component {
            id="plantacle1"
            >
           </Intro>
+          <Role
+            role="Front-end developer, Designer"
+            responsibility="It was my responsibility to read the sensor data in the front-end. I then used this data to write the logic for the different stages of the compost. I have also programmed the tasks that appear based on the stage of the compost. I also styled the website with sass and for the design part I made the high fidelity wireframes and illustrations of the compost bins."
+            build="React.js, Sass"
+          />
           <PhasesPlantacle />
           <Wireframes />
       </Container>
---
+++ src/pages/detail-project/sections/Role.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Role extends React.Component {
+  render(props) {
+    return (
+        <section className="role">
+          <div className="role__text-wrapper">
+              <p className="intro-detail__info"> <b> Role: </b> {this.props.role}</p>
+              <p className="intro-detail__info"> <b> Responsibility: </b> {this.props.responsibility}</p>
+              <p className="intro-detail__info"> <b> Build: </b> {this.props.build}</p>
+          </div>
+        </section>
+    )
+  }
+}
+
+export default Role;
